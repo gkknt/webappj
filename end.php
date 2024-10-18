f96da682d70d6358cd6ec10059fa8fde1f7916ea
pc3 <kyt10@icoud.com>
2024-10-18 17:20:47 +0900

    0

--- end.php
+++ end.php
@@ -4,13 +4,13 @@
 </head>
 <body>
     <p>カードゲーム</p>
-    <a href="start.php"?det=1>
+    <a href="start.php?det=1">
     <img src="kaizoku_man.png">
 </a>
-<a href="start.php"?det=2>
+<a href="start.php?det=2">
     <img src="koudan_man.png">
 </a>
-<a href="start.php"?det=3>
+<a href="start.php?det=3">
     <img src="pet_sanpo_lead.png">
 </a>
 
