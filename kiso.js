a549d0fbe9435c173d65f0f4d89d384651ffbf66
gkknt <kyt10@icloud.com>
2024-10-25 17:33:18 +0900

    d

--- kiso.js
+++ kiso.js
@@ -1,9 +1,9 @@
+
 var w = $('.wrap').width();
 var h = $('.wrap').height();
-$('#wow').attr('width', w);
-$('#wow').attr('height', h);
+$('#canvas').attr('width', w);
+$('#canvas').attr('height', h);
 
-const canvas = document.querySelector('.wow');
+const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-const det='<?=$det?>';
-ctx.filltext(det,100,100);
+
--- start.php
+++ start.php
@@ -2,17 +2,17 @@
 <head lang="ja">
     <meta http-equiv="Content-Type" content="text/html"; chaset="utf8">
     <link rel="stylesheet" type="text/css" href="tmp.css"/>
-    <script src="./kiso.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="kiso.js" defer></script>
     <title>first</title>
 </head>
-<body>
+<body  onload=>
 <div class="wrap">
-    <canvas id="wow" ></canvas>
+    <canvas id="canvas" ></canvas>
 </div>
 
 <?php
 $det=$_GET['det'];
-
 ?>
 
 
